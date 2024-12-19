d2c7b5444d869de670d6271eabb35033059db866
yv1ing <yvling.cn@outlook.com>
2024-12-19 16:27:24 +0800

    new: Add parameters to specify the configuration file

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"flag"
+	"gopkg.in/yaml.v3"
+	"io"
+	"log"
+	"os"
+
 	"MollyBlog/config"
 	"MollyBlog/internal/mApp"
 	"MollyBlog/utils"
 )
 
 func init() {
+	// parse arguments
+	configFilePath := flag.String("c", "config.yaml", "config file")
+
+	flag.Parse()
+
+	// load config
+	configFile, err := os.OpenFile(*configFilePath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer configFile.Close()
+
+	configByte, err := io.ReadAll(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(configByte, &config.MConfigInstance)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create need files
 	postSrc := config.MConfigInstance.Storage.SRC
 	postDst := config.MConfigInstance.Storage.DST
 	utils.Mkdir(postSrc)
@@ -20,5 +49,5 @@ func init() {
 
 func main() {
 	mapp := mApp.NewMApp(config.MConfigInstance)
-	mapp.Run(true)
+	mapp.Run(false)
 }
